f75570e47e34b31266e434c275ae517e01051c31
Edward Bender <ebender001@gmail.com>
2025-02-06 15:46:51 -0800

    Initial Commit

---
+++ UltimatePortfolio9999/UltimatePortfolio9999App.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolio9999App.swift
+//  UltimatePortfolio9999
+//
+//  Created by Edward Bender on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolio9999App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
